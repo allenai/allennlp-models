7ef3cb27ea6839d0bd2a860c4ade35a36cbadf13
Dirk Groeneveld <dirkg@allenai.org>
2020-03-10 15:47:23 -0700

    Moving some QaNet components into this repo

--- allennlp_models/rc/qanet/__init__.py
+++ allennlp_models/rc/qanet/__init__.py
@@ -1,3 +1,5 @@
 from .drop_reader import DropReader
 from .naqanet_model import NumericallyAugmentedQaNet
 from .qanet_model import QaNet
+from .qanet_encoder import QaNetEncoder
+from .qanet_encoder import QaNetEncoderBlock
