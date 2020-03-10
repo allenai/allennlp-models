5219d6cca468b3ebb8762f73af116c147ffa70d2
Dirk Groeneveld <dirkg@allenai.org>
2020-03-10 14:16:16 -0700

    Added bidaf

---
+++ allennlp_models/rc/bidaf/__init__.py
@@ -0,0 +1,4 @@
+from .bidaf_model import BidirectionalAttentionFlow
+from .bidaf_ensemble import BidafEnsemble
+from .bidaf_predictor import ReadingComprehensionPredictor
+from .squad_reader import SquadReader
