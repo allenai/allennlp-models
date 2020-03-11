793158df1aaed0fc2c212ab715050ce17e3225ab
Dirk Groeneveld <dirkg@allenai.org>
2020-03-11 11:53:58 -0700

    Adds the dependency parser predictor from the main repo

--- allennlp_models/syntax/__init__.py
+++ allennlp_models/syntax/__init__.py
@@ -1,2 +1,3 @@
 from .biaffine_dependency_parser import BiaffineDependencyParser
 from .universal_dependencies_reader import UniversalDependenciesDatasetReader
+from .biaffine_dependency_parser_predictor import BiaffineDependencyParserPredictor
