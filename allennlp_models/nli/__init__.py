13a412cf619baba9f18bf344f60b5794488182d9
Dirk Groeneveld <dirkg@allenai.org>
2020-04-08 16:42:38 -0700

    Re-export NLI components

---
+++ allennlp_models/nli/__init__.py
@@ -0,0 +1,5 @@
+from allennlp.models import ESIM
+from allennlp.models import DecomposableAttention
+from allennlp.predictors import DecomposableAttentionPredictor
+
+# These models live in the core repo because we need them there for tests.
