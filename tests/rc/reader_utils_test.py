3d6eecdb4587263a9d7d0af42bc92fa81a8449f0
Dirk Groeneveld <dirkg@allenai.org>
2020-04-24 18:23:33 -0700

    Adds test for the new behavior

--- tests/rc/reader_utils_test.py
+++ tests/rc/reader_utils_test.py
@@ -11,7 +11,13 @@ from allennlp_models.rc.common.reader_utils import char_span_to_token_span
         ([(0, 3), (4, 4), (5, 8)], (0, 8), ((0, 2), False)),
         ([(0, 3), (4, 4), (5, 8)], (1, 8), ((0, 2), True)),
         ([(0, 3), (4, 4), (5, 8)], (7, 8), ((2, 2), True)),
+        ([(0, 3), (4, 4), (5, 8)], (7, 9), ((2, 2), True)),
     ],
 )
 def test_char_span_to_token_span(token_offsets, character_span, expected_result):
     assert char_span_to_token_span(token_offsets, character_span) == expected_result
+
+
+def test_char_span_to_token_span_throws():
+    with pytest.raises(ValueError):
+        char_span_to_token_span([(0, 3), (4, 4), (5, 8)], (7, 19))
