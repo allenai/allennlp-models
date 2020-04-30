289523de1afd76a4773cafc2039b5f203cadb9cc
Dirk Groeneveld <dirkg@allenai.org>
2020-04-29 17:05:35 -0700

    Cap'n Flake to the rescue!

--- setup.py
+++ setup.py
@@ -37,7 +37,9 @@ with open("requirements.txt") as requirements_file:
 
     def fix_url_dependencies(req: str) -> str:
         """Pip and setuptools disagree about how URL dependencies should be handled."""
-        m = re.match(r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req)
+        m = re.match(
+            r"^(git\+)?(https|ssh)://(git@)?github\.com/([\w-]+)/(?P<name>[\w-]+)\.git", req
+        )
         if m is None:
             return req
         else:
