0ea5f41b22d37a3dccbb653f7dcf6f8486ffdd4e
Dirk Groeneveld <dirkg@allenai.org>
2020-06-19 05:00:57 -0700

    Try out optuna

---
+++ allennlp_models/mc/tools/mc_optuna.py
@@ -0,0 +1,38 @@
+import optuna
+
+def objective(trial: optuna.Trial) -> float:
+    trial.suggest_categorical("correct_bias", [True, False])
+    trial.suggest_int('epochs', 1, 5)
+    trial.suggest_int('batch_size', 4, 32)
+    trial.suggest_float('weight_decay', 0, 0.1)
+    trial.suggest_float('lr', 1e-5/3, 5e-5, log=True)
+    trial.suggest_float('cut_frac', 0, 0.1)
+    trial.suggest_float('grad_norm', 0, 2.0)
+
+    executor = optuna.integration.allennlp.AllenNLPExecutor(
+        trial=trial,  # trial object
+        config_file="training_config/mc/piqa-optuna.jsonnet",
+        serialization_dir=f"models/piqa-optuna/{trial.number}",
+        metrics="best_validation_acc",
+        include_package="allennlp_models"
+    )
+    return executor.run()
+
+
+if __name__ == '__main__':
+    study = optuna.create_study(
+        storage=None,
+        sampler=optuna.samplers.TPESampler(seed=24),
+        study_name="piqa-optuna",
+        direction="maximize",
+    )
+
+    timeout = 60 * 60 * 10  # timeout (sec): 60*60*10 sec => 10 hours
+    study.optimize(
+        objective,
+        n_jobs=2,  # number of processes in parallel execution
+        n_trials=60,  # number of trials to train a model
+        timeout=timeout,  # threshold for executing time (sec)
+    )
+
+    optuna.integration.allennlp.dump_best_config("training_config/mc/piqa-optuna.jsonnet", "best_piqa-optuna.json", study)
---
+++ training_config/mc/piqa-optuna.jsonnet
@@ -0,0 +1,47 @@
+local transformer_model = 'roberta-base';
+
+local epochs = std.extVar('epochs');
+local batch_size = std.extVar('batch_size');
+local weight_decay = std.extVar('weight_decay');
+local lr = std.extVar('lr');
+local cut_frac = std.extVar('cut_frac');
+local grad_norm = std.extVar('grad_norm');
+local correct_bias = std.extVar('correct_bias');
+
+{
+  "dataset_reader": {
+      "type": "piqa",
+      "transformer_model_name": transformer_model,
+      //"max_instances": 200  // debug setting
+  },
+  "train_data_path": "https://yonatanbisk.com/piqa/data/train.jsonl",
+  "validation_data_path": "https://yonatanbisk.com/piqa/data/valid.jsonl",
+  "model": {
+      "type": "transformer_mc",
+      "transformer_model_name": transformer_model,
+  },
+  "data_loader": {
+    "sampler": "random",
+    "batch_size": batch_size
+  },
+  "trainer": {
+    "optimizer": {
+      "type": "huggingface_adamw",
+      "weight_decay": weight_decay,
+      "parameter_groups": [[["bias", "LayerNorm\\.weight", "layer_norm\\.weight"], {"weight_decay": 0}]],
+      "lr": lr,
+      "eps": 1e-8,
+      "correct_bias": true
+    },
+    "learning_rate_scheduler": {
+      "type": "slanted_triangular",
+      "cut_frac": cut_frac,
+    },
+    "grad_norm": grad_norm,
+    "num_epochs": epochs,
+    "cuda_device": 0
+  },
+  "random_seed": 42,
+  "numpy_seed": 42,
+  "pytorch_seed": 42,
+}
