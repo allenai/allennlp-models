f71025dcfcc60693685699f01c862b41839764f8
Nelson Liu <nfliu@nelsonliu.me>
2020-06-20 22:49:41 -0700

    Add Squad2 datasetreader and tests

--- allennlp_models/rc/dataset_readers/__init__.py
+++ allennlp_models/rc/dataset_readers/__init__.py
@@ -2,5 +2,6 @@ from allennlp_models.rc.dataset_readers.drop import DropReader
 from allennlp_models.rc.dataset_readers.qangaroo import QangarooReader
 from allennlp_models.rc.dataset_readers.quac import QuACReader
 from allennlp_models.rc.dataset_readers.squad import SquadReader
+from allennlp_models.rc.dataset_readers.squad2 import Squad2Reader
 from allennlp_models.rc.dataset_readers.transformer_squad import TransformerSquadReader
 from allennlp_models.rc.dataset_readers.triviaqa import TriviaQaReader
