f291be5e139431f75f5d938f0ff782408709ac8c
Dirk Groeneveld <dirkg@allenai.org>
2020-07-08 07:39:21 -0700

    Adds a test for the model itself

---
+++ allennlp_models/mc/__init__.py
@@ -0,0 +1,4 @@
+from allennlp_models.mc.dataset_readers.swag import SwagReader
+from allennlp_models.mc.dataset_readers.commonsenseqa import CommonsenseQaReader
+from allennlp_models.mc.dataset_readers.piqa import PiqaReader
+from allennlp_models.mc.models.transformer_mc import TransformerMC
