c694b486035238e1eb64dcf0bc2f4f68a12f3c7e
Hgtc <hgtcwl@foxmail.com>
2020-07-13 23:23:16 +0800

    add test cases

--- tests/generation/models/simple_seq2seq_test.py
+++ tests/generation/models/simple_seq2seq_test.py
@@ -27,6 +27,12 @@ class SimpleSeq2SeqTest(ModelTestCase):
             self.param_file, tolerance=1e-2, overrides=param_overrides
         )
 
+    def test_multi_layer_decoder_model_can_train_save_and_load(self):
+        param_overrides = json.dumps({"model": {"target_decoder_layers": 2}})
+        self.ensure_model_can_train_save_and_load(
+            self.param_file, tolerance=1e-2, overrides=param_overrides
+        )
+
     def test_no_attention_model_can_train_save_and_load(self):
         param_overrides = json.dumps({"model": {"attention": None}})
         self.ensure_model_can_train_save_and_load(
@@ -40,7 +46,6 @@ class SimpleSeq2SeqTest(ModelTestCase):
         )
 
     def test_loss_is_computed_correctly(self):
-
         batch_size = 5
         num_decoding_steps = 5
         num_classes = 10
@@ -76,7 +81,6 @@ class SimpleSeq2SeqTest(ModelTestCase):
         assert "predicted_tokens" in decode_output_dict
 
     def test_greedy_decode_matches_beam_search(self):
-
         beam_search = BeamSearch(
             self.model._end_index, max_steps=self.model._max_decoding_steps, beam_size=1
         )
