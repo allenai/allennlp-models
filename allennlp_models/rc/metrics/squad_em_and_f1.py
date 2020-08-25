6d724f28724a45f7841b0c0971a0b26983394540
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-08-25 14:13:17 -0400

    distributed metrics: SquadEmAndF1

--- allennlp_models/rc/metrics/squad_em_and_f1.py
+++ allennlp_models/rc/metrics/squad_em_and_f1.py
@@ -1,5 +1,9 @@
 from typing import Tuple
 
+import torch
+import torch.distributed as dist
+
+from allennlp.common.util import is_distributed
 from allennlp.training.metrics.metric import Metric
 from overrides import overrides
 
@@ -33,6 +37,19 @@ class SquadEmAndF1(Metric):
         f1_score = squad.metric_max_over_ground_truths(
             squad.f1_score, best_span_string, answer_strings
         )
+        if is_distributed():
+            if dist.get_backend() == "nccl":
+                device = torch.cuda.current_device()
+            else:
+                device = torch.device("cpu")
+            # Converting bool to float here, since we want to count the number of exact matches.
+            _exact_match = torch.tensor(exact_match, dtype=torch.float).to(device)
+            _f1_score = torch.tensor(f1_score).to(device)
+            dist.all_reduce(_exact_match, op=dist.ReduceOp.SUM)
+            dist.all_reduce(_f1_score, op=dist.ReduceOp.SUM)
+            exact_match = _exact_match.item()
+            f1_score = _f1_score.item()
+
         self._total_em += exact_match
         self._total_f1 += f1_score
         self._count += 1
