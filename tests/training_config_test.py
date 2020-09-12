2ef9da50baf5faca8e87d482976f6c62dc9675db
Dirk Groeneveld <dirkg@allenai.org>
2020-09-11 17:03:22 -0700

    Brings back the test for TransformerElmo

--- tests/training_config_test.py
+++ tests/training_config_test.py
@@ -14,7 +14,6 @@ from allennlp.common.plugins import import_plugins
 
 CONFIGS_TO_IGNORE = {
     # TODO (epwalsh): once the new data loading API is merged, try to get this model working.
-    "bidirectional_language_model.jsonnet",
     # Requires some bi-directional LM archive path.
     "constituency_parser_transformer_elmo.jsonnet",
 }
@@ -35,12 +34,10 @@ def find_configs():
 
 
 GLOVE_PATCHES = {
-    FIXTURES_ROOT
-    / "glove.6B.100d.sample.txt.gz": (
+    FIXTURES_ROOT / "glove.6B.100d.sample.txt.gz": (
         "https://allennlp.s3.amazonaws.com/datasets/glove/glove.6B.100d.txt.gz",
     ),
-    FIXTURES_ROOT
-    / "glove.6B.300d.sample.txt.gz": (
+    FIXTURES_ROOT / "glove.6B.300d.sample.txt.gz": (
         "https://allennlp.s3.amazonaws.com/datasets/glove/glove.6B.300d.txt.gz",
         "https://allennlp.s3.amazonaws.com/datasets/glove/glove.840B.300d.txt.gz",
         "https://allennlp.s3.amazonaws.com/datasets/glove/glove.840B.300d.lower.converted.zip",
@@ -59,54 +56,32 @@ def patch_glove(params):
 
 
 DATASET_PATCHES: Dict[Path, Tuple[str, ...]] = {
-    FIXTURES_ROOT
-    / "structured_prediction"
-    / "srl"
-    / "conll_2012": ("SRL_TRAIN_DATA_PATH", "SRL_VALIDATION_DATA_PATH"),
-    FIXTURES_ROOT
-    / "structured_prediction"
-    / "example_ptb.trees": ("PTB_TRAIN_PATH", "PTB_DEV_PATH", "PTB_TEST_PATH"),
-    FIXTURES_ROOT
-    / "structured_prediction"
-    / "dependencies.conllu": ("PTB_DEPENDENCIES_TRAIN", "PTB_DEPENDENCIES_VAL"),
-    FIXTURES_ROOT
-    / "structured_prediction"
-    / "semantic_dependencies"
-    / "dm.sdp": ("SEMEVAL_TRAIN", "SEMEVAL_DEV", "SEMEVAL_TEST"),
+    FIXTURES_ROOT / "structured_prediction" / "srl" / "conll_2012": ("SRL_TRAIN_DATA_PATH", "SRL_VALIDATION_DATA_PATH"),
+    FIXTURES_ROOT / "structured_prediction" / "example_ptb.trees": ("PTB_TRAIN_PATH", "PTB_DEV_PATH", "PTB_TEST_PATH"),
+    FIXTURES_ROOT / "structured_prediction" / "dependencies.conllu": ("PTB_DEPENDENCIES_TRAIN", "PTB_DEPENDENCIES_VAL"),
+    FIXTURES_ROOT / "structured_prediction" / "semantic_dependencies" / "dm.sdp": ("SEMEVAL_TRAIN", "SEMEVAL_DEV", "SEMEVAL_TEST"),
     FIXTURES_ROOT / "tagging" / "conll2003.txt": ("NER_TRAIN_DATA_PATH", "NER_TEST_DATA_PATH"),
     FIXTURES_ROOT / "mc" / "swag.csv": ("SWAG_TRAIN", "SWAG_DEV", "SWAG_TEST"),
     FIXTURES_ROOT / "rc" / "drop.json": ("DROP_TRAIN", "DROP_DEV"),
-    FIXTURES_ROOT / "lm" / "language_model" / "sentences.txt": ("BIDIRECTIONAL_LM_TRAIN_PATH",),
     FIXTURES_ROOT / "rc" / "squad.json": ("SQUAD_TRAIN", "SQUAD_DEV"),
-    FIXTURES_ROOT
-    / "coref"
-    / "coref.gold_conll": (
+    FIXTURES_ROOT / "coref" / "coref.gold_conll": (
         "COREF_TRAIN_DATA_PATH",
         "COREF_DEV_DATA_PATH",
         "COREF_TEST_DATA_PATH",
     ),
-    FIXTURES_ROOT
-    / "structured_prediction"
-    / "srl"
-    / "conll_2012"
-    / "subdomain": ("CONLL_TRAIN_DATA_PATH", "CONLL_DEV_DATA_PATH"),
-    FIXTURES_ROOT
-    / "tagging"
-    / "conll2003.txt": (
+    FIXTURES_ROOT / "structured_prediction" / "srl" / "conll_2012" / "subdomain": ("CONLL_TRAIN_DATA_PATH", "CONLL_DEV_DATA_PATH"),
+    FIXTURES_ROOT / "tagging" / "conll2003.txt": (
         "NER_TRAIN_DATA_PATH",
         "NER_TEST_DATA_PATH",
         "NER_TEST_A_PATH",
         "NER_TEST_B_PATH",
     ),
-    FIXTURES_ROOT
-    / "generation"
-    / "bart"
-    / "data"
-    / "url_lists"
-    / "all_train.txt": (
+    FIXTURES_ROOT / "generation" / "bart" / "data" / "url_lists" / "all_train.txt": (
         "CNNDM_TRAIN",
         "CNNDM_DEV",
     ),
+    FIXTURES_ROOT / "lm" / "bidirectional_language_model" / "vocab": ("BIDIRECTIONAL_LM_VOCAB_PATH",),
+    FIXTURES_ROOT / "lm" / "bidirectional_language_model" / "training_data" / "*": ("BIDIRECTIONAL_LM_TRAIN_PATH",),
 }
 
 
@@ -128,6 +103,7 @@ class TestAllenNlpPretrainedModelConfigs(AllenNlpTestCase):
             path,
             params_overrides="{"
             "'trainer.cuda_device': -1, "
+            "'trainer.use_amp': false, "
             "'trainer.num_epochs': 2, "
             "'dataset_reader.max_instances': 4, "
             "'dataset_reader.lazy': false, "
