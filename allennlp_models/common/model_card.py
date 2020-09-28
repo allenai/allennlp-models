dd368903a85a4590da5db92c17d1a429c15760a3
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-09-28 15:49:32 -0700

    Adding more documentation to model cards spec (#137)
    * adding missing newline
    
    * adding more documentation to model card spec
    
    Co-authored-by: Dirk Groeneveld <dirkg@allenai.org>

--- allennlp_models/common/model_card.py
+++ allennlp_models/common/model_card.py
@@ -1,3 +1,19 @@
+"""
+A specification for defining model cards as described in
+[Model Cards for Model Reporting (Mitchell et al, 2019)]
+(https://api.semanticscholar.org/CorpusID:52946140)
+
+The descriptions of the fields and some examples
+are taken from the paper.
+
+The specification is provided to prompt model developers
+to think about the various aspects that should ideally
+be reported. The information filled should adhere to
+the spirit of transparency rather than the letter; i.e.,
+it should not be filled for the sake of being filled. If
+the information cannot be inferred, it should be left empty.
+"""
+
 import os
 import logging
 from dataclasses import dataclass
@@ -42,6 +58,60 @@ class ModelCardInfo(FromParams):
 class ModelDetails(ModelCardInfo):
     """
     This provides the basic information about the model.
+
+    # Parameters:
+
+    description: `str`
+        A high-level overview of the model.
+        Eg. The model implements a reading comprehension model patterned
+            after the proposed model in [Devlin et al, 2018]
+            (https://api.semanticscholar.org/CorpusID:52967399), with improvements
+            borrowed from the SQuAD model in the transformers project.
+            It predicts start tokens and end tokens with a linear layer on top of
+            word piece embeddings.
+
+    developed_by: `str`
+        Person/organization that developed the model. This can be used by all
+        stakeholders to infer details pertaining to model development and
+        potential conflicts of interest.
+
+    contributed_by: `str`
+        Person that contributed the model to the repository.
+
+    date: `str`
+        The date on which the model was contributed. This is useful for all
+        stakeholders to become further informed on what techniques and
+        data sources were likely to be available during model development.
+        Format example: 2020-09-23
+
+    version: `str`
+        The version of the model, and how it differs from previous versions.
+        This is useful for all stakeholders to track whether the model is the
+        latest version, associate known bugs to the correct model versions,
+        and aid in model comparisons.
+
+    model_type: `str`
+        The type of the model; the basic architecture. This is likely to be
+        particularly relevant for software and model developers, as well as
+        individuals knowledgeable about machine learning, to highlight what
+        kinds of assumptions are encoded in the system.
+        Eg. Naive Bayes Classifier.
+
+    paper: `str`
+        The paper on which the model is based.
+        Format example:
+        [Model Cards for Model Reporting (Mitchell et al, 2019)]
+        (https://api.semanticscholar.org/CorpusID:52946140)
+
+    citation: `str`
+        The BibTex for the paper.
+
+    license: `str`
+        License information for the model.
+
+    contact: `str`
+        The email address to reach out to the relevant developers/contributors
+        for questions/feedback about the model.
     """
 
     description: Optional[str] = None
@@ -59,8 +129,33 @@ class ModelDetails(ModelCardInfo):
 @dataclass(frozen=True)
 class IntendedUse(ModelCardInfo):
     """
-    This determines what the model should and should not be
-    used for.
+    This determines what the model should and should not be used for.
+
+    # Parameters:
+
+    primary_uses: `str`
+        Details the primary intended uses of the model; whether it was developed
+        for general or specific tasks.
+        Eg. The toxic text identifier model was developed to identify
+            toxic comments on online platforms. An example use case is
+            to provide feedback to comment authors.
+
+    primary_users: `str`
+        The primary intended users. For example, was the model developed
+        for entertainment purposes, for hobbyists, or enterprise solutions?
+        This helps users gain insight into how robust the model may be to
+        different kinds of inputs.
+
+    out_of_scope_use_cases: `str`
+        Highlights the technology that the model might easily be confused with,
+        or related contexts that users could try to apply the model to.
+        Eg. the toxic text identifier model is not intended for fully automated
+            moderation, or to make judgements about specific individuals.
+
+        Also recommends a related or similar model that was designed to better
+        meet a particular need, where possible.
+        Eg. not for use on text examples longer than 100 tokens; please use
+        the bigger-toxic-text-identifier instead.
     """
 
     primary_uses: Optional[str] = None
@@ -74,6 +169,22 @@ class Factors(ModelCardInfo):
     This provides a summary of relevant factors such as
     demographics, instrumentation used, etc. for which the
     model performance may vary.
+
+    # Parameters:
+
+    relevant_factors: `str`
+         The foreseeable salient factors for which model performance may vary,
+         and how these were determined.
+         Eg. the model performance may vary for variations in dialects of English.
+
+    evaluation_factors: `str`
+        Mentions the factors that are being reported, and the reasons for why
+        they were chosen. Also includes the reasons for choosing different
+        evaluation factors than relevant factors.
+
+        Eg. While dialect variation is a relevant factor,
+        dialect-specific annotations were not available, and hence, the
+        performance was not evaluated on different dialects.
     """
 
     relevant_factors: Optional[str] = None
@@ -85,6 +196,20 @@ class Metrics(ModelCardInfo):
     """
     This lists the reported metrics and the reasons
     for choosing them.
+
+    # Parameters:
+
+    model_performance_measures: `str`
+        Which model performance measures were selected and the reasons for
+        selecting them.
+    decision_thresholds: `str`
+        If decision thresholds are used, what are they, and the reasons for
+        choosing them.
+    variation_approaches: `str`
+        How are the measurements and estimations of these metrics calculated?
+        Eg. standard deviation, variance, confidence intervals, KL divergence.
+        Details of how these values are approximated should also be included.
+        Eg. average of 5 runs, 10-fold cross-validation, etc.
     """
 
     model_performance_measures: Optional[str] = None
@@ -96,6 +221,19 @@ class Metrics(ModelCardInfo):
 class EvaluationData(ModelCardInfo):
     """
     This provides information about the evaluation data.
+
+    # Parameters:
+
+    dataset: `str`
+        The name(s) (and link(s), if available) of the dataset(s) used to evaluate
+        the model. Optionally, provide a link to the relevant datasheet(s) as well.
+    motivation: `str`
+        The reasons for selecting the dataset(s).
+        Eg. For the BERT model, document-level corpora were used rather than a
+            shuffled sentence-level corpus in order to extract long contiguous sequences.
+    preprocessing: `str`
+        How was the data preprocessed for evaluation?
+        Eg. tokenization of sentences, filtering of paragraphs by length, etc.
     """
 
     dataset: Optional[str] = None
@@ -113,7 +251,28 @@ class EvaluationData(ModelCardInfo):
 @dataclass(frozen=True)
 class TrainingData(ModelCardInfo):
     """
-    This provides information about the training data.
+    This provides information about the training data. If the model was initialized
+    from pretrained weights, a link to the pretrained model's model card/training
+    data can additionally be provided, if available. Any relevant definitions should
+    also be included.
+
+    # Parameters:
+
+    dataset: `str`
+        The name(s) (and link(s), if available) of the dataset(s) used to train
+        the model. Optionally, provide a link to the relevant datasheet(s) as well.
+        Eg. * Proprietary data from Perspective API; includes comments from online
+              forums such as Wikipedia and New York Times, with crowdsourced labels of
+              whether the comment is "toxic".
+            * "Toxic" is defined as "a rude, disrespectful, or unreasonable comment
+              that is likely to make you leave a discussion."
+    motivation: `str`
+        The reasons for selecting the dataset(s).
+        Eg. For the BERT model, document-level corpora were used rather than a
+            shuffled sentence-level corpus in order to extract long contiguous sequences.
+    preprocessing: `str`
+        Eg. Only the text passages were extracted from English Wikipedia;  lists, tables,
+            and headers were ignored.
     """
 
     dataset: Optional[str] = None
@@ -132,7 +291,18 @@ class TrainingData(ModelCardInfo):
 class QuantitativeAnalyses(ModelCardInfo):
     """
     This provides disaggregated evaluation of how the
-    model performed based on chosen metrics.
+    model performed based on chosen metrics, with confidence
+    intervals, if possible. Links to plots/figures showing
+    the metrics can also be provided.
+
+    # Parameters:
+
+    unitary_results: `str`
+        The performance of the model with respect to each chosen
+        factor.
+    intersectional_results: `str`
+        The performance of the model with respect to the intersection
+        of the evaluated factors.
     """
 
     unitary_results: Optional[str] = None
@@ -148,6 +318,9 @@ class EthicalConsiderations(ModelCardInfo):
     decisions on human life? Does it use sensitive data?
     What kind of risks are possible, and what mitigation
     strategies were used to address them?
+    Eg. The model does not take into account user history
+        when making judgments about toxicity, due to privacy
+        concerns.
     """
 
     ethical_considerations: Optional[str] = None
@@ -156,8 +329,11 @@ class EthicalConsiderations(ModelCardInfo):
 @dataclass(frozen=True)
 class CaveatsAndRecommendations(ModelCardInfo):
     """
-    This lists any additional concerns. Eg. were any relevant
-    groups not present in the evaluation data?
+    This lists any additional concerns. For instance, were any
+    relevant groups not present in the evaluation data?
+    Eg. The evaluation data is synthetically designed to be
+        representative of common use cases and concerns, but
+        may not be comprehensive.
     """
 
     caveats_and_recommendations: Optional[str] = None
@@ -165,9 +341,7 @@ class CaveatsAndRecommendations(ModelCardInfo):
 
 class ModelCard(ModelCardInfo):
     """
-    The model card stores the recommended attributes for model reporting
-    as described in the paper
-    [Model Cards for Model Reporting (Mitchell et al, 2019)](https://arxiv.org/pdf/1810.03993.pdf).
+    The model card stores the recommended attributes for model reporting.
 
     # Parameters
 
