3ecbe962355f8626d78f10a9071246287202b8da
epwalsh <epwalsh10@gmail.com>
2020-11-04 11:48:23 -0800

    set seed instead of ignoring bias parameters

--- tests/rc/models/naqanet_test.py
+++ tests/rc/models/naqanet_test.py
@@ -1,5 +1,3 @@
-from flaky import flaky
-
 from allennlp.common.testing import ModelTestCase
 
 from tests import FIXTURES_ROOT
@@ -11,15 +9,10 @@ class NumericallyAugmentedQaNetTest(ModelTestCase):
     def setup_method(self):
         super().setup_method()
         self.set_up_model(
-            FIXTURES_ROOT / "rc" / "naqanet" / "experiment.json", FIXTURES_ROOT / "rc" / "drop.json"
+            FIXTURES_ROOT / "rc" / "naqanet" / "experiment.json",
+            FIXTURES_ROOT / "rc" / "drop.json",
+            seed=15,
         )
 
-    @flaky(max_runs=3, min_passes=1)
     def test_model_can_train_save_and_load(self):
-        self.ensure_model_can_train_save_and_load(
-            self.param_file,
-            gradients_to_ignore={
-                "_passage_span_end_predictor._linear_layers.1.bias",
-                "_question_span_end_predictor._linear_layers.1.bias",
-            },
-        )
+        self.ensure_model_can_train_save_and_load(self.param_file)
