617824bc547856ebe140a02ea3acfd8bf369285d
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-12-15 11:48:04 -0800

    enabling logging

--- scripts/ensure_models_found.py
+++ scripts/ensure_models_found.py
@@ -3,10 +3,12 @@
 """
 Ensures models are automatically found by allennlp.
 """
+import logging
 
 from allennlp.common.plugins import import_plugins
 from allennlp.models import Model
 
+logging.basicConfig(level=logging.INFO)
 
 import_plugins()
 Model.by_name("copynet_seq2seq")
