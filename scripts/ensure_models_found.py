321bf60f5a27349d7587258d61110dddd652f268
Akshita Bhagia <akshita23bhagia@gmail.com>
2020-12-15 12:00:55 -0800

    temporary change for debugging

--- scripts/ensure_models_found.py
+++ scripts/ensure_models_found.py
@@ -8,6 +8,8 @@ import logging
 from allennlp.common.plugins import import_plugins
 from allennlp.models import Model
 
+import allennlp_models #Should ideally fail with some error.
+
 logging.basicConfig(level=logging.INFO)
 
 import_plugins()
