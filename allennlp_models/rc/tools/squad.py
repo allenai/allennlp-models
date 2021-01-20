9472904acd94f6fca168713a6183ed6ecd2449cf
Evan Pete Walsh <epwalsh10@gmail.com>
2021-01-20 12:59:38 -0800

    Fix TransformerQA distributed dead-lock bug (#193)
    * modify training config for repro
    
    * fix TransformerQA distributed dead-lock bug
    
    * revert config changes
    
    * handle different sized batches
    
    * comments
    
    * fix test
    
    Co-authored-by: Dirk Groeneveld <dirkg@allenai.org>

--- allennlp_models/rc/tools/squad.py
+++ allennlp_models/rc/tools/squad.py
@@ -6,6 +6,7 @@ for SQuAD version 2.0.
 import collections
 import re
 import string
+from typing import Callable, Sequence, TypeVar, Tuple
 
 
 def make_qid_to_has_ans(dataset):
@@ -43,27 +44,34 @@ def get_tokens(s):
     return normalize_answer(s).split()
 
 
-def compute_exact(a_pred, a_gold):
+def compute_exact(a_pred: str, a_gold: str) -> int:
     return int(normalize_answer(a_pred) == normalize_answer(a_gold))
 
 
-def compute_f1(a_pred, a_gold):
+def compute_f1(a_pred: str, a_gold: str) -> float:
     pred_toks = get_tokens(a_pred)
     gold_toks = get_tokens(a_gold)
-    common = collections.Counter(pred_toks) & collections.Counter(gold_toks)
+    common = collections.Counter(pred_toks) & collections.Counter(gold_toks)  # type: ignore[var-annotated]
     num_same = sum(common.values())
     if len(pred_toks) == 0 or len(gold_toks) == 0:
         # If either is no-answer, then F1 is 1 if they agree, 0 otherwise
-        return int(pred_toks == gold_toks)
+        return float(pred_toks == gold_toks)
     if num_same == 0:
-        return 0
+        return 0.0
     precision = 1.0 * num_same / len(pred_toks)
     recall = 1.0 * num_same / len(gold_toks)
     f1 = (2 * precision * recall) / (precision + recall)
     return f1
 
 
-def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
+_P = TypeVar("_P")
+_G = TypeVar("_G")
+_T = TypeVar("_T", int, float, Tuple[int, ...], Tuple[float, ...])
+
+
+def metric_max_over_ground_truths(
+    metric_fn: Callable[[_P, _G], _T], prediction: _P, ground_truths: Sequence[_G]
+) -> _T:
     scores_for_ground_truths = []
     for ground_truth in ground_truths:
         score = metric_fn(prediction, ground_truth)
@@ -71,7 +79,7 @@ def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
     return max(scores_for_ground_truths)
 
 
-def get_metric_score(prediction, gold_answers):
+def get_metric_score(prediction: str, gold_answers: Sequence[str]) -> Tuple[int, float]:
     exact_scores = metric_max_over_ground_truths(compute_exact, prediction, gold_answers)
     f1_scores = metric_max_over_ground_truths(compute_f1, prediction, gold_answers)
     return exact_scores, f1_scores
