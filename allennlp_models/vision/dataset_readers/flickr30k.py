d55cf7350e42d67adb5cf6c1fd296541a3de4c1e
Jacob Morrison <jacob1morrison@gmail.com>
2021-04-23 10:22:44 -0700

    adding sentence parser

--- allennlp_models/vision/dataset_readers/flickr30k.py
+++ allennlp_models/vision/dataset_readers/flickr30k.py
@@ -26,6 +26,57 @@ from allennlp.modules.vision.grid_embedder import GridEmbedder
 from allennlp.modules.vision.region_detector import RegionDetector
 from allennlp_models.vision.dataset_readers.vision_reader import VisionReader
 
+# Borrowed
+# parse sentence file for a given image
+def get_sentence_data(fn):
+    with open(fn, 'r') as f:
+        sentences = f.read().split('\n')
+
+    annotations = []
+    for sentence in sentences:
+        if not sentence:
+            continue
+
+        first_word = []
+        phrases = []
+        phrase_id = []
+        phrase_type = []
+        words = []
+        current_phrase = []
+        add_to_phrase = False
+        for token in sentence.split():
+            if add_to_phrase:
+                if token[-1] == ']':
+                    add_to_phrase = False
+                    token = token[:-1]
+                    current_phrase.append(token)
+                    phrases.append(' '.join(current_phrase))
+                    current_phrase = []
+                else:
+                    current_phrase.append(token)
+
+                words.append(token)
+            else:
+                if token[0] == '[':
+                    add_to_phrase = True
+                    first_word.append(len(words))
+                    parts = token.split('/')
+                    phrase_id.append(parts[1][3:])
+                    phrase_type.append(parts[2:])
+                else:
+                    words.append(token)
+
+        sentence_data = {'sentence' : ' '.join(words), 'phrases' : []}
+        for index, phrase, p_id, p_type in zip(first_word, phrases, phrase_id, phrase_type):
+            sentence_data['phrases'].append({'first_word_index' : index,
+                                             'phrase' : phrase,
+                                             'phrase_id' : p_id,
+                                             'phrase_type' : p_type})
+
+        annotations.append(sentence_data)
+
+    return annotations
+
 
 @DatasetReader.register("flickr30k")
 class Flickr30kReader(VisionReader):
@@ -94,7 +145,61 @@ class Flickr30kReader(VisionReader):
     # todo: implement
     @overrides
     def _read(self, split_or_filename: str):
-        pass
+    @overrides
+    def _read(self, file_path: str):
+        # if the splits are using slicing syntax, honor it
+        slice_match = re.match(r"(.*)\[([0123456789:]*)]", file_path)
+        if slice_match is None:
+            question_slice = slice(None, None, None)
+        else:
+            split_name = slice_match[1]
+            slice_args = [int(a) if len(a) > 0 else None for a in slice_match[2].split(":")]
+            question_slice = slice(*slice_args)
+
+        file_path = cached_path(file_path.split("[")[0], extract_archive=True)
+
+        # todo: change to work with the flickr dataset
+        logger.info("Reading file at %s", file_path)
+        questions = []
+        with open(file_path) as dataset_file:
+            dataset = json.load(dataset_file)
+        for data in dataset:
+            for qa in data["qas"]:
+                questions.append(qa)
+        questions = questions[question_slice]
+
+        question_dicts = list(self.shard_iterable(questions))
+
+        # todo: probably move this to a utils file?
+        processed_images: Iterable[Optional[Tuple[Tensor, Tensor]]]
+        if self.produce_featurized_images:
+            # It would be much easier to just process one image at a time, but it's faster to process
+            # them in batches. So this code gathers up instances until it has enough to fill up a batch
+            # that needs processing, and then processes them all.
+
+            filenames = [f"{question_dict['image_id']}.jpg" for question_dict in question_dicts]
+            # for filename in filenames:
+                # logger.info("Reading file at %s", filename)
+                # logger.info("Reading file at %s", self.images[filename])
+            logger.info("images size: %s", len(self.images))
+            try:
+                processed_images = self._process_image_paths(
+                    self.images[filename] for filename in filenames
+                )
+            except KeyError as e:
+                print(self.images)
+                missing_id = e.args[0]
+                raise KeyError(
+                    missing_id,
+                    f"We could not find an image with the id {missing_id}. "
+                    "Because of the size of the image datasets, we don't download them automatically. "
+                    "Please go to https://visualqa.org/download.html, download the datasets you need, "
+                    "and set the image_dir parameter to point to your download location. This dataset "
+                    "reader does not care about the exact directory structure. It finds the images "
+                    "wherever they are.",
+                )
+        else:
+            processed_images = [None for _ in range(len(question_dicts))]
 
     # todo: implement
     @overrides
