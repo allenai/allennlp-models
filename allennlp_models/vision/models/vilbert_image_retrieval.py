b3c3a1a451e86ec06a9a0e848682038e7e7b348c
Jacob Morrison <jacob1morrison@gmail.com>
2021-05-05 10:56:54 -0700

    debug statement

--- allennlp_models/vision/models/vilbert_image_retrieval.py
+++ allennlp_models/vision/models/vilbert_image_retrieval.py
@@ -176,7 +176,9 @@ class ImageRetrievalVilbert(VisionTextModel):
         # idea: the correct image for a caption i is image_i
         labels = torch.from_numpy(np.arange(0, batch_size))
 
+        print(labels.device)
         labels.to(outputs["logits"].device)
+        print(labels.device)
 
         outputs["loss"] = torch.nn.functional.cross_entropy(outputs["logits"], labels) / batch_size
         self.accuracy(outputs["logits"], labels)
