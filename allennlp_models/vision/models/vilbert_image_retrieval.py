c51efd035e015f99c33cb9f2105427f94be4de70
Jacob Morrison <jacob1morrison@gmail.com>
2021-05-06 15:53:51 -0700

    fix

--- allennlp_models/vision/models/vilbert_image_retrieval.py
+++ allennlp_models/vision/models/vilbert_image_retrieval.py
@@ -140,7 +140,7 @@ class ImageRetrievalVilbert(VisionTextModel):
         text_embeddings = backbone_outputs["encoded_text_pooled"]
         # Shape: (batch_size, pooled_output_dim)
         # image_embeddings = backbone_outputs["encoded_boxes_pooled"]
-        image_embedding = torch.mean(backbone_outputs["sequence_output_v"], dim=1)
+        image_embedding = torch.mean(backbone_outputs["encoded_boxes"], dim=1)
 
         # TODO: do stuff with this
         # Shape: (batch_size, batch_size)
