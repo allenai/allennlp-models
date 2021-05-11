9ce28a4659defd6322246209a0157ab1276aab33
Jacob Morrison <jacob1morrison@gmail.com>
2021-05-11 15:59:29 -0700

    debug

--- allennlp_models/vision/dataset_readers/nlvr2.py
+++ allennlp_models/vision/dataset_readers/nlvr2.py
@@ -27,6 +27,7 @@ def extract_image_features(image: Union[str, Tuple[Tensor, Tensor]], use_cache: 
         features, coords = image
 
     print(features.shape)
+
     return (
         ArrayField(features),
         ArrayField(coords),
@@ -200,7 +201,7 @@ class Nlvr2Reader(VisionReader):
         use_cache: bool = True,
     ) -> Instance:
         hypothesis_field = TextField(self._tokenizer.tokenize(hypothesis), None)
-        box_features1, box_coordinates1, box_mask1 = extract_image_features(image1, use_cache)
+        box_features1, box_coordinates1, box_mask1 = extract_image_features(image1, False) # use_cache)
         box_features2, box_coordinates2, box_mask2 = extract_image_features(image2, use_cache)
 
         fields = {
