76e5195efb04953227cc45cff7f825918a9256bd
Jacob Morrison <jacob1morrison@gmail.com>
2021-05-14 21:50:44 -0700

    trying choices instead of sample

--- allennlp_models/vision/dataset_readers/flickr30k.py
+++ allennlp_models/vision/dataset_readers/flickr30k.py
@@ -17,7 +17,7 @@ import torch
 from torch import Tensor
 import faiss
 import transformers
-from random import sample
+from random import sample, choices
 
 from allennlp.common.file_utils import cached_path
 from allennlp.common.lazy import Lazy
@@ -225,7 +225,10 @@ class Flickr30kReader(VisionReader):
             processed_images = [None for _ in range(len(caption_dicts))]
 
         hard_negatives_cache = {}
-        for caption_dict, filename, processed_image in zip(caption_dicts, filenames, processed_images):
+        # for caption_dict, filename, processed_image in zip(caption_dicts, filenames, processed_images):
+        for i in range(len(caption_dicts)):
+            caption_dict = caption_dicts[i]
+
             for caption in caption_dict["captions"]:
                 instance = self.text_to_instance(
                     caption, filename, processed_image, processed_images, hard_negatives_cache
@@ -345,7 +348,9 @@ class Flickr30kReader(VisionReader):
         heapq.heapify(heap)
         # TODO: see if we don't have to sample? the cache might not be working for hard negatives
         print(len(other_images))
-        for image in other_images: # sample(other_images, min(len(other_images), 100)):
+        sampled_images = choices(other_images, k=500)
+        print(len(sampled_images))
+        for image in sampled_images: # other_images: # sample(other_images, min(len(other_images), 100)):
             # Calculate the 3 closest hard negatives:
             # 1. Calculate mean of all boxes
             # 2. Find the ~100 nearest neighbors of the input image
