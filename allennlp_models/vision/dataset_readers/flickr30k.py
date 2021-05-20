6ec16732c44b808920265ba29b8a7888badbfd32
Jacob Morrison <jacob1morrison@gmail.com>
2021-05-20 15:54:59 -0700

    deleting batch?

--- allennlp_models/vision/dataset_readers/flickr30k.py
+++ allennlp_models/vision/dataset_readers/flickr30k.py
@@ -408,6 +408,7 @@ class Flickr30kReader(VisionReader):
                 )
                 # Shape: (1, 1024)
                 caption_embedding = self.model(**batch).pooler_output.squeeze(0).cpu()
+                del batch
                 curr_captions.append(caption_embedding)
             caption_list.append(torch.stack(curr_captions, dim=0))
         # Shape: (num_captions, 5, 1024)
