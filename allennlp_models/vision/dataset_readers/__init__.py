a8f9ef2f8b1932c05d9505033f7023696f3f0b91
Dirk Groeneveld <dirkg@allenai.org>
2021-06-09 16:39:50 -0700

    Make the reader visible

--- allennlp_models/vision/dataset_readers/__init__.py
+++ allennlp_models/vision/dataset_readers/__init__.py
@@ -2,3 +2,4 @@ from allennlp_models.vision.dataset_readers.vision_reader import VisionReader
 from allennlp_models.vision.dataset_readers.gqa import GQAReader
 from allennlp_models.vision.dataset_readers.vqav2 import VQAv2Reader
 from allennlp_models.vision.dataset_readers.visual_entailment import VisualEntailmentReader
+from allennlp_models.vision.dataset_readers.flickr30k import Flickr30kReader
