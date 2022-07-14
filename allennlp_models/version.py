bac6505ba801e1033d405cd7bc03faa3243de5c0
epwalsh <epwalsh10@gmail.com>
2022-07-14 12:46:56 -0700

    Prepare for release v2.10.0

--- allennlp_models/version.py
+++ allennlp_models/version.py
@@ -1,8 +1,8 @@
 import os
 
 _MAJOR = "2"
-_MINOR = "9"
-_PATCH = "3"
+_MINOR = "10"
+_PATCH = "0"
 # This is mainly for nightly builds which have the suffix ".dev$DATE". See
 # https://semver.org/#is-v123-a-semantic-version for the semantics.
 _SUFFIX = os.environ.get("ALLENNLP_MODELS_VERSION_SUFFIX", "")
